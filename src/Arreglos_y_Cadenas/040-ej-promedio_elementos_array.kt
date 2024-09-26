7c990b5495adff3c7840e590d87adfed5212d855
JesusThoro <2021545@utsh,edu,mx>
2024-09-26 06:00:49 -0600

    4

---
+++ src/Arreglos_y_Cadenas/040-ej-promedio_elementos_array.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val numeros = doubleArrayOf(10.0, 20.0, 30.0, 40.0, 50.0)
+    val promedio = numeros.average()
+    println("El promedio de los elementos del array es: $promedio")
+}
